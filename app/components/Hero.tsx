46780c76e40e23e3352a0457d967b575e832d121
Harshit <harshitmalik29@gmail.com>
2025-09-10 23:41:49 +0530

    starting off with homepage

---
+++ app/components/Hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+    return (
+        <div className="flex justify-center items-center">
+            <div className="flex flex-col items-center color-black">
+            Understand anything
+                
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div className="bg-white min-h-screen">
       <Header />
+      <Hero />
     </div>
   );
 }
