c9702eeaabebae297e44f6d99b4161bd6d6eb67a
Harshit <harshitmalik29@gmail.com>
2025-09-11 23:12:26 +0530

    added chat storiage for session to create unique session ids for chat

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { ConversationAgent } from '@/app/agents/conversationAgent';
+import { timeStamp } from 'console';
 let chatStore = new Map<string, any> ();
 
 export async function POST(request: NextRequest){
@@ -18,4 +19,31 @@ export async function POST(request: NextRequest){
             { status: 400 }
         );
     }
+
+    const chatId = `chat_${Date.now()}_${Math.random().toString(36).substring(2, 9)}`;
+    
+    const agent = new ConversationAgent();
+
+    const initialResponse = await agent.process(initialMessage.trim());
+
+    const chatSession = [
+        id: chatId,
+        message: [
+            {
+                text: initialMessage.trim(),
+                isUser: true,
+                timeStamp: Date.now()
+            },
+            {
+                text: initialResponse,
+                isUser: false,
+                timeStamp: Date.now()
+            }
+        ],
+        context: agent.getContext(),
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    ]
+
+    chatStore.set(chatId, chatSession);
 }
